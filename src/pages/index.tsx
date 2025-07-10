19841c0ebb6042dbb7cf2b3a47e790d1169dfdda
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-10 16:04:25 +0530

    Add: correct index.tsx for root redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-
-"import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Index() {
   const router = useRouter();
+
   useEffect(() => {
-    router.push('/login');
+    router.push("/login");
   }, [router]);
 
   return null;
