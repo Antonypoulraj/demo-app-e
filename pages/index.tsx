cddf23f2571606db16892e02b6442831f6215b3f
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-11 15:15:48 +0530

    Fix case-sensitive route: /login -> /Login

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 export default function Index() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/login");
+    router.push("/Login");
   }, [router]);
   return null;
 }
