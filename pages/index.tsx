4c6df701c53775da0c59aa5da8f1df576e50f577
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-11 15:58:29 +0530

    Fix case sensitivity: Rename Dashboard.tsx to dashboard.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 export default function Index() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/Login");
+    router.push("/login");
   }, [router]);
   return null;
 }
