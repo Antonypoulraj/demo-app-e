c889e74dd818180e1016d150bd0803b177c51874
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-12 14:56:38 +0530

    Refactor: Use singleton Prisma client across all API routes

--- pages/api/employees/[id].ts
+++ pages/api/employees/[id].ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
--- pages/api/rawmaterials/[id].ts
+++ pages/api/rawmaterials/[id].ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma";
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
