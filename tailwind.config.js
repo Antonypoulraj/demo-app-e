348a219b7c853c5e95b881398361375eec2cb78f
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-15 12:04:28 +0530

    update:remove src and app content in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./contexts/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}", // ✅ add if using app dir
-    "./src/**/*.{js,ts,jsx,tsx}", // ✅ add if using src/ folder
   ],
 
   theme: {
