d46f5073b31a5f98b8230c985dfda8a7be0ed78b
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-16 14:48:46 +0530

    Fix: Apply Geist font variables to <html> in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
+import { geistSans, geistMono } from "./_app"; // OR re-import here if necessary
 
 export default function Document() {
   return (
-    <Html>
-      <Head>{/* Preload Geist font manually if needed */}</Head>
-      <body className="font-sans">
+    <Html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Head />
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
