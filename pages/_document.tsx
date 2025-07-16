960b025749ad1dea1430190426d2cb9600c1d42d
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-16 14:44:12 +0530

    update:remove the link content in the _document configuration in the pages folder

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,16 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        {/* Preload Geist font manually if needed */}
-        <link
-          rel="preload"
-          href="/fonts/your-font.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
+      <Head>{/* Preload Geist font manually if needed */}</Head>
       <body className="font-sans">
         <Main />
         <NextScript />
