85bdf197796de83fa089488ae0386c9838a68caf
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-16 10:56:58 +0530

    fix: include styles and src in Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./contexts/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css}",
   ],
   theme: {
     extend: {
