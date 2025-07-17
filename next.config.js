ccfebfc929119a5b2f3c8b6af2d08b0e363fd6df
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-17 15:01:33 +0530

    Setup shadcn and updated config, styles, and utils

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   experimental: {
+//     serverActions: true, // Optional for Next.js 14
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverActions: true, // Optional for Next.js 14
-  },
 };
 
 module.exports = nextConfig;
