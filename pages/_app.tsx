020b08ece11c1f8b1f35ce99bd69ae24aaa38774
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-17 15:31:22 +0530

    update:modify the import to down

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/contexts/AuthContext";
