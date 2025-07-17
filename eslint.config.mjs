6cdcc321ae06cae5d78277d6ad3fe1fe654505a9
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-17 12:35:45 +0530

    chore: migrate to ESLint flat config with TypeScript, Next.js, and Prettier support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
-// eslint.config.mjs
-import js from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
-import next from "eslint-plugin-next";
-import prettier from "eslint-config-prettier";
+const js = require("@eslint/js");
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const tsparser = require("@typescript-eslint/parser");
+const next = require("eslint-plugin-next");
+const prettier = require("eslint-config-prettier");
 
-export default [
+/** @type {import("eslint").Linter.FlatConfig[]} */
+module.exports = [
   js.configs.recommended,
   {
     files: ["**/*.{js,ts,jsx,tsx}"],
@@ -29,6 +29,38 @@ export default [
   prettier,
   next.configs.recommended,
 ];
+
+// // eslint.config.mjs
+// import js from "@eslint/js";
+// import tseslint from "@typescript-eslint/eslint-plugin";
+// import tsparser from "@typescript-eslint/parser";
+// import next from "eslint-plugin-next";
+// import prettier from "eslint-config-prettier";
+
+// export default [
+//   js.configs.recommended,
+//   {
+//     files: ["**/*.{js,ts,jsx,tsx}"],
+//     languageOptions: {
+//       parser: tsparser,
+//       parserOptions: {
+//         project: "./tsconfig.json",
+//         ecmaVersion: "latest",
+//         sourceType: "module",
+//       },
+//     },
+//     plugins: {
+//       "@typescript-eslint": tseslint,
+//       next: next,
+//     },
+//     rules: {
+//       "@typescript-eslint/no-unused-vars": "off",
+//       "react/no-unescaped-entities": "off",
+//     },
+//   },
+//   prettier,
+//   next.configs.recommended,
+// ];
 
 // import { dirname } from "path";
 // import { fileURLToPath } from "url";
